1f4c0336735b30f7d89fb1c2ede76f724e29749e
Dubovik DA <gamma15alpha@gmail.com>
2024-12-18 10:34:38 +0300

    try spotbugsMain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "1.9.0"
+	kotlin("plugin.spring") version "1.9.0"
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
-	id("io.gitlab.arturbosch.detekt") version "1.23.1"
-	kotlin("kapt") version "1.9.25"
+	kotlin("kapt") version "1.9.0"
+	id("com.github.spotbugs") version "5.0.14"
 }
 
 group = "com.kp2"
@@ -63,7 +63,6 @@ dependencies {
 	// Devtools для разработки
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
-
 
 kotlin {
 	compilerOptions {
